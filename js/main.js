cab0e4731670633c83abcdd831f9b5edf5b78b4f
aefe2 <shigaev_1990@mail.ru>
2023-01-30 10:21:22 +0700

    Практическая 3

--- js/main.js
+++ js/main.js
@@ -6,5 +6,8 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
     }
 })
