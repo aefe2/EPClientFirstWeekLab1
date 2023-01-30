baafbde2d4df460350ee0b6faf9da357e0157f53
aefe2 <shigaev_1990@mail.ru>
2023-01-30 09:48:14 +0700

    Знакомство с v-bind, вывод носка в html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
+        description: "A pair of warm, fuzzy socks.",
+        image: "./assets/vmSocks-green-onWhite.jpg",
     }
 })
