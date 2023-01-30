6a8313ed291eaa8ee77bfdc881e807b205faacee
aefe2 <shigaev_1990@mail.ru>
2023-01-30 10:38:31 +0700

    Практическая 4

--- js/main.js
+++ js/main.js
@@ -9,5 +9,17 @@ let app = new Vue({
         inStock: true,
         inventory: 100,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     }
 })
