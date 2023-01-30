5a499da75b87304dfa532946d651315e012256a6
aefe2 <shigaev_1990@mail.ru>
2023-01-30 10:07:32 +0700

    Практическая 2

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@ let app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks.",
         image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     }
 })
