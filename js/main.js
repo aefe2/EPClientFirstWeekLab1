78c7a4e28f558b7b8c8e92a356ea1e0b72a0b1b3
aefe2 <shigaev_1990@mail.ru>
2023-01-30 11:21:27 +0700

    Практическая 6, добавить к Out of stock стиль line-through, если inStock - false

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
-        inventory: 100,
+        inStock: false,
+        inventory: 0,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
